8a935d2958b6ea4dd8d6295975fa0b3493f645b8
Lucas Altenhofen <lucas.a.bertrand@gmail.com>
2021-06-16 00:12:22 -0300

    Delete transactions

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
 // import AppError from '../errors/AppError';
 
+import { getRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
+
+interface DeleteTransactionDTO {
+  id: string;
+}
+
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: DeleteTransactionDTO): Promise<void> {
+    const repository = getRepository(Transaction);
+
+    await repository.delete(id);
   }
 }
 
