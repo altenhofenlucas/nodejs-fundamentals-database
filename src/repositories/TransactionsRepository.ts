aa3c06f29ee429e7aa1aca4e4332f80c1fbbdcce
Lucas Altenhofen <lucas.a.bertrand@gmail.com>
2021-06-16 00:02:24 -0300

    List transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,27 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, currentValue) => {
+        const { type, value } = currentValue;
+
+        if (type === 'income') {
+          accumulator.income += value;
+        } else {
+          accumulator.outcome += value;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    return { income, outcome, total: income - outcome };
   }
 }
 
